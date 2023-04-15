ac83843d28d6db8a8734ce8b5f8826d170d02831
Unique Shrestha <112570655+Unique-py@users.noreply.github.com>
2023-04-15 20:26:54 +0545

    Update Objective2.3.c

--- Labsheet-2/Objective2.3.c
+++ Labsheet-2/Objective2.3.c
@@ -1,3 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x,y;
+clrscr();
+printf("enter the value of x and y :");
+scanf("%d%d",&x,&y);
+if(x>y)
+printf("x is greater than y");
+else
+printf("Y is greater than x ");
+getch();
+	
+}
+/* output 
+enter the value of x and y :5 6
+Y is greater than x*/
+
+
+
 //Program to find if entered number is even or odd
 #include<stdio.h>
 #include<conio.h>
