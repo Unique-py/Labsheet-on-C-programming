df1ffca153b59294d494d6ec3e3b22baf9975df4
Unique Shrestha <112570655+Unique-py@users.noreply.github.com>
2023-04-15 20:24:02 +0545

    Create Objective2.3.c

---
+++ Labsheet-2/Objective2.3.c
@@ -0,0 +1,14 @@
+//Program to find if entered number is even or odd
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int n;
+printf("Enter a number ");
+scanf("%d",&n);
+if(n%2==0)
+printf("%d is even",n);
+else
+printf("%d is odd ",n);
+	
+}
