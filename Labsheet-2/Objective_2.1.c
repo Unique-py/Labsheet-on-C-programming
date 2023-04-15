ad3d81001815f5cd488d7620ba2cce9eb373fa8b
Unique Shrestha <112570655+Unique-py@users.noreply.github.com>
2023-04-15 20:14:03 +0545

    Update Objective_2.1.c

--- Labsheet-2/Objective_2.1.c
+++ Labsheet-2/Objective_2.1.c
@@ -1,1 +1,18 @@
-helo
+#include<stdio.h>
+#include<conio.h>
+main()
+{ 
+  int x,y;
+  clrscr();
+  printf("Enter the value of x:");
+  scanf("%d",&x);
+  if(x>0) y=1;
+  if(x==0) y=-1;
+  printf("\n y=%d",y);
+  getch();
+}
+
+
+/* Output 
+Enter the value of x:4
+ y=1 */
