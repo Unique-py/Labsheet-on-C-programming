f8289dcd6753bb405e4e02fa8cbfc4c67b22a65f
Unique Shrestha <112570655+Unique-py@users.noreply.github.com>
2023-04-26 17:36:21 +0545

    Create Objective_1.2.c

---
+++ Labsheet#3/Objective_1.2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int marks;
+    printf("enter the marks of a student ");
+    scanf("%d",&marks);
+    if(marks<32)
+        printf("fail");
+    else if(marks < 45 || marks == 45)
+        printf("Third Division");
+    else if(marks < 60 || marks == 60)
+        printf("Second Division");
+    else
+        printf("First Division");
+    getch();
+    return 0;
+}
