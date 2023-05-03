b75900a8832bfd93b7a9ba7f75b88e744e4fa79d
Unique Shrestha <112570655+Unique-py@users.noreply.github.com>
2023-05-03 13:58:45 +0545

    Create 1.1.c

---
+++ Labsheet-4/Objective.#1/1.1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int v=1;
+	printf("%d \n",v)
+	v = v+1;
+	printf("%d \n",v)
+	v = v+1;
+	printf("%d \n",v)
+	v = v+1;
+	printf("%d \n",v)
+	v = v+1;
+	printf("%d \n",v)
+	v = v+1;
+}
