95572c8715df07964fc6b049d5b91a114527d25b
Unique Shrestha <112570655+Unique-py@users.noreply.github.com>
2023-05-03 14:46:10 +0545

    Create 2.5.c

---
+++ Labsheet-4/Objective#2/2.5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+main()
+{       
+    int i,n,fact=1;
+	
+	printf("Input the 10 numbers : \n");
+	scanf("%d",&n);
+	
+	for (i=1;i<=10;i++)
+	{
+		fact = fact * i;
+		
+		
+	}
+	printf("%d",sum);
+ 
+}
