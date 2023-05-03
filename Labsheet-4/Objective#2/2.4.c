5c5cb74e438b5adc98dbc806744ca9789f87181e
Unique Shrestha <112570655+Unique-py@users.noreply.github.com>
2023-05-03 14:45:47 +0545

    Update 2.4.c

--- Labsheet-4/Objective#2/2.4.c
+++ Labsheet-4/Objective#2/2.4.c
@@ -1,1 +1,18 @@
 
+#include <stdio.h>
+main()
+{       
+    int i,n,sum=0;
+	
+	printf("Input the 10 numbers : \n");
+	scanf("%d",&n);
+	
+	for (i=1;i<=10;i++)
+	{
+		sum += i;
+		
+		
+	}
+	printf("%d",sum);
+ 
+}
